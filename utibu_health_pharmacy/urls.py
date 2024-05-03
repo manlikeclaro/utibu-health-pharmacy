8274f22eb5abb45d54e813d44587b67893d35271
manlikeclaro <ckibii08@gmail.com>
2024-05-03 15:25:46 +0300

    [FEAT] devised method to serve static & media files directly in production

--- utibu_health_pharmacy/urls.py
+++ utibu_health_pharmacy/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 from utibu_health_pharmacy import views
 
@@ -17,5 +18,11 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     # In production, static files should be served by Whitenoise or a dedicated web server
-    # Media files should be served by the web server directly
+
+    # Serve static and media files using regex paths in production
+    # urlpatterns += [
+    #     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    #     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    # ]
+
     pass
